82a6f02e276c56762c06181e42e2dc2aa10d0907
Michael Fluharty <michael.fluharty@mac.com>
2025-10-16 12:08:49 -0500

    Initial Commit

---
+++ Tally Matrix Clock/Tally_Matrix_ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tally_Matrix_ClockApp.swift
+//  Tally Matrix Clock
+//
+//  Created by Michael Fluharty on 10/16/25.
+//
+
+import SwiftUI
+
+@main
+struct Tally_Matrix_ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Tally Matrix ClockTests/Tally_Matrix_ClockTests.swift
@@ -0,0 +1,17 @@
+//
+//  Tally_Matrix_ClockTests.swift
+//  Tally Matrix ClockTests
+//
+//  Created by Michael Fluharty on 10/16/25.
+//
+
+import Testing
+@testable import Tally_Matrix_Clock
+
+struct Tally_Matrix_ClockTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
